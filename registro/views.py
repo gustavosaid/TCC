654637fc71aa5bd7307b56e187ec7271cdadfa20
Gustavo GS. Said <gustavosaid@scmpm.local>
2025-10-28 11:33:19 -0300

    remover TK

--- registro/views.py
+++ registro/views.py
@@ -15,7 +15,7 @@ from django.conf import settings
 import tempfile
 from django.core.files import File
 from django.core.files.base import ContentFile
-from tkinter import simpledialog
+
 from django.utils import timezone
 from .forms import ColetaEntradaForms
 from django.urls import reverse
